608fafd668a15def4fc47dcd2fc93b45e5813739
pranave2003 <pranav2003a10@gmail.com>
2024-02-14 11:10:51 +0530

    first

---
+++ lib/d1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Dash extends StatefulWidget {
+  const Dash({super.key});
+
+  @override
+  State<Dash> createState() => _DashState();
+}
+
+class _DashState extends State<Dash> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
